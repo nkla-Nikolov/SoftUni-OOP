1757edb59a9689177470fd91eb84317e326b7e0a
nkla-Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2021-11-23 21:38:44 +0200

    added tasks
    jiji

---
+++ Encapsulation-Exercise/FootballTeamGenerator/Team.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+    public class Team
+    {
+        private string name;
+        private List<Player> players;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            players = new List<Player>();
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+
+                name = value;
+            }
+        }
+        public int Rating => (int)Math.Round(players.Average(x => x.AverageStat));
+
+
+        public void AddPlayer(Player player)
+        {
+            players.Add(player);
+        }
+
+        public void RemovePlayer(Player player)
+        {
+            if (player == null)
+            {
+                throw new ArgumentException();
+            }
+
+            players.Remove(player);
+        }
+    }
+}
---
+++ Encapsulation-Exercise/PizzaCalories/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] pizzainfo = Console.ReadLine().Split();
+                string[] doughInfo = Console.ReadLine().Split();
+                Dough dough = new Dough(doughInfo[1], doughInfo[2], double.Parse(doughInfo[3]));
+                Pizza pizza = new Pizza(pizzainfo[1], dough);
+
+                string toppingInput = Console.ReadLine();
+
+                while (toppingInput != "END")
+                {
+                    string[] toppingInfo = toppingInput.Split();
+                    string toppingType = toppingInfo[1];
+                    double weight = double.Parse(toppingInfo[2]);
+
+                    Topping topping = new Topping(toppingType, weight);
+                    pizza.Add(topping);
+
+                    toppingInput = Console.ReadLine();
+                }
+                
+                Console.WriteLine($"{pizza.Name} - {pizza.TotalCalories:f2} Calories.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
