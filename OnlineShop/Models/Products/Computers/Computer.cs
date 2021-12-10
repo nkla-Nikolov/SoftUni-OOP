650c96f6157be2219bfdf1d475fa03f120be2c93
nkla-Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2021-12-10 15:45:21 +0200

    OOP Exam - 16 August 2020

---
+++ OnlineShop/Models/Products/Computers/Computer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OnlineShop.Models.Products.Components;
+using OnlineShop.Models.Products.Peripherals;
+
+namespace OnlineShop.Models.Products.Computers
+{
+    public abstract class Computer : Product, IComputer
+    {
+        private decimal price;
+        private double overallPerformance;
+        private List<IComponent> components;
+        private List<IPeripheral> peripherals;
+
+
+        protected Computer(int id, string manufacturer, string model, decimal price, double overallPerformance)
+            : base(id, manufacturer, model, price, overallPerformance)
+        {
+            this.components = new List<IComponent>();
+            this.peripherals = new List<IPeripheral>();
+            this.OverallPerformance = overallPerformance;
+            this.Price = price;
+        }
+
+        public double OverallPerformance
+        {
+            get => overallPerformance;
+            private set
+            {
+                if (components.Count == 0)
+                {
+                    overallPerformance = value;
+                }
+                else
+                {
+                    double averageComponentsPerformance = components.Sum(x => x.OverallPerformance) / components.Count;
+                    overallPerformance = value + averageComponentsPerformance;
+                }
+            }
+        }
+
+        public decimal Price
+        {
+            get => price;
+            private set
+            {
+                price = value + components.Sum(x => x.Price) + peripherals.Sum(x => x.Price);
+            }
+        }
+
+        public IReadOnlyCollection<IComponent> Components => components.AsReadOnly();
+
+        public IReadOnlyCollection<IPeripheral> Peripherals => peripherals.AsReadOnly();
+
+
+        public void AddComponent(IComponent component)
+        {
+            if (components.Any(x => x.GetType().Name == component.GetType().Name))
+            {
+                throw new ArgumentException(
+                    $"Component {component.GetType().Name} already exists in {this.GetType().Name} with Id {this.Id}.");
+            }
+
+            components.Add(component);
+        }
+
+        public IComponent RemoveComponent(string componentType)
+        {
+            var component = components.FirstOrDefault(x => x.GetType().Name == componentType);
+
+            if (component == null)
+            {
+                throw new ArgumentException(
+                    $"Component {componentType} does not exist in {this.GetType().Name} with Id {this.Id}."
+                );
+            }
+
+            components.Remove(component);
+            return component;
+        }
+
+        public void AddPeripheral(IPeripheral peripheral)
+        {
+            if (peripherals.Any(x => x.GetType().Name == peripheral.GetType().Name))
+            {
+                throw new ArgumentException($"Peripheral {peripheral.GetType().Name} already exists in {this.GetType().Name} with Id {this.Id}."
+                );
+            }
+
+            peripherals.Add(peripheral);
+        }
+
+        public IPeripheral RemovePeripheral(string peripheralType)
+        {
+            var peripheral = peripherals.FirstOrDefault(x => x.GetType().Name == peripheralType);
+
+            if (peripheral == null)
+            {
+                throw new ArgumentException($"Peripheral {peripheralType} does not exist in {this.GetType().Name} with Id {this.Id}."
+                );
+            }
+
+            peripherals.Remove(peripheral);
+            return peripheral;
+        }
+    }
+}
