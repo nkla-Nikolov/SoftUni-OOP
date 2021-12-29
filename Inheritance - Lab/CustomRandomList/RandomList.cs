342997ca6cf5dae98ba4b93a32ea37db5cd127fe
Nikola Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2021-12-29 18:05:07 +0200

    initial commit

---
+++ Inheritance - Lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+        private List<string> list;
+
+        public RandomList()
+        {
+            list = new List<string>();
+        }
+        public void Remove()
+        {
+            int index = rnd.Next(0, Count);
+            list.RemoveAt(index);
+        }
+
+        public string RandomString()
+        {
+            int index = rnd.Next(0, Count);
+            return list[index];
+        }
+    }
+}
---
+++ Inheritance - Lab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        private Stack<string> stack;
+
+        public StackOfStrings()
+        {
+            stack = new Stack<string>();
+        }
+
+        public bool IsEmpty()
+        {
+            return stack.Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> items)
+        {
+            foreach (var item in items)
+            {
+                stack.Push(item);
+            }
+        }
+    }
+}
