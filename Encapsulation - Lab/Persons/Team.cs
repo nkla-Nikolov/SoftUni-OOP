25742e17e1fea234b942a26513d7bc9e53ad0a59
Nikola Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2021-12-30 21:51:35 +0200

    initial commit

---
+++ Encapsulation - Lab/Persons/Team.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace PersonsInfo.Persons
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get => firstTeam.AsReadOnly();
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get => reserveTeam.AsReadOnly();
+        }
+
+        public string Name { get; private set; }
+
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
